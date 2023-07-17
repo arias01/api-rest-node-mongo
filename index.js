a9322014f0acd7ce9532e06d2b41d69acd88402e
bitsalbert <64283411+bitsalbert@users.noreply.github.com>
2023-07-16 22:38:47 -0400

    Adding server configurations and Runing

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 const { connection } = require("./database/connection")
+const express  = require("express")
+const cors = require("cors")
+
 console.log("Hi, Is Running");
 
+//database connection
 connection();
+const port=3900;
+//creating node server
+const app =express();
+
+//Cors Configuration with midleware
+app.use(cors());
+
+//Converting body to js object auto
+app.use(express.json());
+
+//Creating routes
+
+//Creating server en listening 
+app.listen(port, ()=>{
+    console.log(`App Running on port ${port}`)
+});
